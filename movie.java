7a662dbf809250e1ec78dee37f661cc6d1858adf
mduc2110 <62826110+mduc2110@users.noreply.github.com>
2020-07-27 00:42:16 +0700

    commit buoi toi

--- movie.java
+++ movie.java
@@ -5,15 +5,17 @@ public class movie{
 		System.out.println("password: ");
 		System.out.println("----------");
 		System.out.println("welcome to booking movie ticket!");
+		System.out.println("=======================================================");
+		System.out.println("1.Booking || 2. Change pass || 3.View history || 4.Exit");
+		System.out.println("Choose: 1");
 		System.out.println("Please choose the day you want: 27/7/2020");
-		System.out.println("----------------------------------------------------");
+		System.out.println("=======================================================");
 		System.out.println("*27/7/2020:");
 		System.out.println("1.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4 || seat 3/100");
-		System.out.println("1.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4 || seat 3/100");
-		System.out.println("1.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4 || seat 3/100");
-		System.out.println("1.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4 || seat 3/100");
+		System.out.println("2.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4 || seat 3/100");
+		System.out.println("3.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4 || seat 3/100");
+		System.out.println("4.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4 || seat 3/100");
 		System.out.println("There are films in 27/7/2020! Choose a compatible film number: 1");
-		System.out.println("Choose a seat: E5");
 		System.out.println("*-1-2-3-4-5-6-7-8-9");
 		System.out.println("A-o-o-o-o-o-o-o-o-o");
 		System.out.println("B-o-o-o-o-o-o-o-o-o");
@@ -21,10 +23,62 @@ public class movie{
 		System.out.println("D-o-x-x-x-o-o-o-o-o");
 		System.out.println("E-o-o-o-o-o-o-o-o-o");
 		System.out.println("F-o-o-o-o-o-o-o-o-o");
+		System.out.println("Choose a seat: E5");
+		System.out.println("Here is your history booking: ");
+		System.out.println("1.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4 || E5");
+		System.out.println("Continue? (Y/N): Y");
+		
+		System.out.println("=======================================================");
+		System.out.println("1.Booking || 2. Change pass || 3.View history || 4.Exit");
+		System.out.println("Choose: 2");
+		System.out.println("Input your old password: 123456");
+		System.out.println("Input your new password: aaaaaa");
+		System.out.println("Your password has changed successfully!");
+		System.out.println("Continue? (Y/N): N");
+		System.out.println("Good bye!");
+		
+		System.out.println("Welcome admin!");
+		System.out.println("======================================");
+		System.out.println("1.CRUD Movie || 2. CRUD User || 3.Exit");		
+		System.out.println("Choose: 1");
+		System.out.println("================================================");
+		System.out.println("1.Add a film: || 2. Delete film || 3.Update film");		
+		System.out.println("Choose: 1");
+		System.out.println("Input name of film: ");
+		System.out.println("Input theater: ");
+		System.out.println("Input time start: ");
+		System.out.println("Input time end: ");
+		System.out.println("Successfully!");
+		System.out.println("Continue in CRUD Movie (Y/N)?: Y");
+		System.out.println("================================================");
+		System.out.println("1.Add a film: || 2. Delete film || 3.Update film");		
+		System.out.println("Choose: 2");
+		System.out.println("Here is a list of film. Choose a compatible number to delete: 2");
+		System.out.println("1.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4|");
+		System.out.println("2.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4");
+		System.out.println("3.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4");
+		System.out.println("4.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4");
+		System.out.println("Deleted successfully!");
+		System.out.println("Continue in CRUD Movie (Y/N)?: Y");
+		System.out.println("================================================");
+		System.out.println("1.Add a film: || 2. Delete film || 3.Update film");		
+		System.out.println("Choose: 3");
+		System.out.println("Here is a list of film. Choose a compatible number to update: 3");
+		System.out.println("1.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4");
+		System.out.println("2.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4");
+		System.out.println("3.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4");
+		System.out.println("4.Avengers4 || from 13:00:00 to 16:00:00 || theater: 4");
+		System.out.println("Input name of film: ");
+		System.out.println("Input theater: ");
+		System.out.println("Input time start: ");
+		System.out.println("Input time end: ");
+		System.out.println("Successfully!");
+		System.out.println("Continue in CRUD Movie (Y/N)?: N");
+		System.out.println("Continue (Y/N)?: Y");
 		
 		
-		
-		
+		System.out.println("======================================");
+		System.out.println("1.CRUD Movie || 2. CRUD User || 3.Exit");
 		/*Scanner scanner = new Scanner(System.in);
 		System.out.println("Input your seat: ");
 		String seat = scanner.next();
